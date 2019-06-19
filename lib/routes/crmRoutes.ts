e67b4d6336223835d274ee042f093a750d8ce6aa
marceloccs <marcelo.custodio95@gmail.com>
2019-06-18 23:01:24 -0300

    inicio das implementações modules e controllers

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -1,12 +1,42 @@
 import {Request, Response} from "express";
+import { ContactController } from "../controllers/crmController";
 
-export class Routes {       
-    public routes(app): void {          
+export class Routes {   
+    public contactController: ContactController = new ContactController();    
+    public routes(app): void {   
+        
         app.route('/')
         .get((req: Request, res: Response) => {            
             res.status(200).send({
                 message: 'GET request successfulll!!!!'
             })
-        })               
+        })
+        
+        // Contact 
+        app.route('/contact')
+        .get(this.contactController.getContacts)
+        .post(this.contactController.addNewContact);
+
+        // Contact detail
+        app.route('/contact/:contactId')
+        // get specific contact
+        .get((req: Request, res: Response) => {
+        // Get a single contact detail            
+            res.status(200).send({
+                message: 'GET request successfulll!!!!'
+            })
+        })
+        .put((req: Request, res: Response) => {
+        // Update a contact           
+            res.status(200).send({
+                message: 'PUT request successfulll!!!!'
+            })
+        })
+        .delete((req: Request, res: Response) => {       
+        // Delete a contact     
+            res.status(200).send({
+                message: 'DELETE request successfulll!!!!'
+            })
+        })
     }
 }
